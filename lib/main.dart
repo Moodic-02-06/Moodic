bc886a5dbbbdd2abad545fe9f0791f2d8919cc00
01025641396-netizen <01025641396@gmail.com>
2026-02-06 19:53:05 +0900

    feat:파일추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_moodic/firebase_options.dart';
+import 'package:flutter_moodic/presentatin/pares/home_page/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,6 +15,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: HomePage());
   }
 }
