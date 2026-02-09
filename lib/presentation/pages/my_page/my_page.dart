1795098899467ec68269ae720e648d92d91387a1
지은성 <ddrr2852@gmail.com>
2026-02-09 16:16:57 +0900

    feat : 마이페이지 UI 구현 완

--- lib/presentation/pages/my_page/my_page.dart
+++ lib/presentation/pages/my_page/my_page.dart
@@ -6,11 +6,85 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("마이페이지")),
-      body: SafeArea(child: Column(children: [
-            
+      appBar: AppBar(
+        title: Text("프로필"),
+        actions: [
+          Icon(Icons.edit),
+          //
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+                SizedBox(width: 12),
+                // 닉네임,소개
+                Column(
+                  children: [
+                    Text("Maenggo"), Text("반갑습니다"),
+                    //
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 12),
+            SizedBox(width: double.infinity, child: Text("이달의 감정 리포트")),
+            //감정 그래프
+            Container(
+              width: double.infinity,
+              height: 171,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.purple,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(width: 34, height: 90, color: Colors.red),
+                  Container(width: 34, height: 120, color: Colors.orange),
+                  Container(width: 34, height: 80, color: Colors.yellow),
+                  Container(width: 34, height: 140, color: Colors.green),
+                  Container(width: 34, height: 80, color: Colors.blue),
+                  Container(width: 34, height: 60, color: Colors.pink),
+                ],
+              ),
+              //
+            ),
+            SizedBox(height: 12),
+            GridView.builder(
+              // 그리드뷰 높이를 자식만큼 줄여서 높이값을 지정해줌
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                crossAxisCount: 2,
+              ),
+              itemCount: 12,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey,
+                  ),
+                );
+              },
+            ),
           ],
-        )),
+        ),
+      ),
     );
   }
 }
---
+++ lib/presentation/pages/my_page/my_page_edit.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class MyPageEdit extends StatefulWidget {
+  const MyPageEdit({super.key});
+
+  @override
+  State<MyPageEdit> createState() => _MyPageEditState();
+}
+
+class _MyPageEditState extends State<MyPageEdit> {
+  bool _isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("프로필 편집")),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Container(width: 100, height: 100, color: Colors.grey),
+              ),
+              Icon(Icons.camera_alt),
+              SizedBox(height: 12),
+              Text("닉네임"),
+              SizedBox(height: 12),
+              Container(
+                padding: EdgeInsets.all(12),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text('홍길동', textAlign: TextAlign.center),
+              ),
+              SizedBox(height: 12),
+              Text(
+                "2~12자의 한글, 영문, 숫자만 사용가능 합니다",
+                style: TextStyle(fontSize: 10),
+              ),
+              SizedBox(height: 12),
+              Container(width: double.infinity, height: 1, color: Colors.grey),
+              SizedBox(height: 12),
+              Text("소개"),
+              SizedBox(height: 12),
+              Container(
+                padding: EdgeInsets.all(24),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              SizedBox(height: 12),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Text("알림", textAlign: TextAlign.center),
+                  ),
+                  Spacer(),
+                  //토글버튼
+                  Switch(
+                    value: _isSwitched,
+                    onChanged: (value) {
+                      setState(() {
+                        _isSwitched = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
