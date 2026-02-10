12cbd8928042ea6fc7ade72e8181f8eca61ec530
01025641396-netizen <01025641396@gmail.com>
2026-02-10 16:00:49 +0900

    feat:디테일페이지 ui

--- lib/domain/entity/user_entity.dart
+++ lib/domain/entity/user_entity.dart
@@ -1,5 +1,13 @@
 class UserEntity {
   final String uid;
+  final String nickname;
+  final String? profileImage;
+  final String? bio;
 
-  UserEntity({required this.uid});
+  UserEntity({
+    required this.uid,
+    required this.nickname,
+    required this.profileImage,
+    required this.bio,
+  });
 }
