959cef27cc500a263cd9da7e8e4031dc323eda69
01025641396-netizen <01025641396@gmail.com>
2026-02-12 20:49:53 +0900

    feat:mypage 구현

--- lib/presentation/pages/my_page/my_page_viewmodel.dart
+++ lib/presentation/pages/my_page/my_page_viewmodel.dart
@@ -1,10 +1,29 @@
+import 'package:flutter_moodic/domain/entity/post.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyPageState {}
+class MyPageState {
+  final List<Post> feeds;
+  final bool isLoading;
+  final String? errorMessage;
+
+  MyPageState({required this.feeds, this.isLoading = false, this.errorMessage});
+
+  MyPageState copyWith({
+    List<Post>? feeds,
+    bool? isLoading,
+    String? errorMessage,
+  }) {
+    return MyPageState(
+      feeds: feeds ?? this.feeds,
+      isLoading: isLoading ?? this.isLoading,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+}
 
 class MyPageViewmodel extends Notifier<MyPageState> {
   @override
   MyPageState build() {
-    return MyPageState();
+    return MyPageState(feeds: []);
   }
 }
